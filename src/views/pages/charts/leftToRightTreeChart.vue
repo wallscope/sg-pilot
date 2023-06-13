aa9ce38095f5ac00f822a2e664113dd71df8ff74
MichalHoinca <michal.hoinca@wallscope.co.uk>
2023-06-13 15:52:11 +0100

    initial detailed graph - example data

--- src/views/pages/charts/leftToRightTreeChart.vue
+++ src/views/pages/charts/leftToRightTreeChart.vue
@@ -1,45 +1,84 @@
 <script setup lang="ts">
-// import { Measurement, MeasurementType, useMeasurementStore } from '@/stores/measurements';
-// import { useMetadataStore } from '@/stores/metadata';
-// import { useSummaryStore } from '@/stores/summary';
-import { BarChart } from "echarts/charts";
-import {
-GridComponent, LegendComponent, TitleComponent,
-TooltipComponent
-} from "echarts/components";
+import { ref, onMounted, Ref } from "vue";
+import { GridComponent, LegendComponent, TitleComponent, TooltipComponent, } from "echarts/components";
 import { use } from "echarts/core";
+import { TreeChart } from 'echarts/charts'
 import { CanvasRenderer } from "echarts/renderers";
-import type { ECBasicOption } from "echarts/types/dist/shared";
-import { ref, type Ref } from "vue";
 import Chart from "vue-echarts";
+import type { ECBasicOption } from "echarts/types/dist/shared";
 
-use([
-  CanvasRenderer,
-  BarChart,
-  GridComponent,
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-]);
+use([GridComponent, LegendComponent, TitleComponent, TooltipComponent, TreeChart, CanvasRenderer]);
 
-// let hide = true;
 let hide = false;
+var ROOT_PATH = "";
+const data = ref(null);
 
-// Chart
-type ChartOptions = ECBasicOption & { xAxis: any; yAxis: any };
+const chartOptions: Ref<ECBasicOption | null> = ref(null);
 
+onMounted(async () => {
+  try {
+    const response = await fetch(ROOT_PATH + "/detailed_example.json");
+    const jsonData = await response.json();
+
+    jsonData.children.forEach(function (
+      datum: { collapsed: boolean },
+      index: number
+    ) {
+      index % 2 === 0 && (datum.collapsed = true);
+    });
+
+    data.value = jsonData;
+
+    chartOptions.value = {
+      tooltip: {
+        trigger: "item",
+        triggerOn: "mousemove",
+      },
+      series: [
+        {
+          type: "tree",
+          data: [data.value],
+          top: "1%",
+          left: "7%",
+          bottom: "1%",
+          right: "20%",
+          symbolSize: 7,
+          label: {
+            position: "left",
+            verticalAlign: "middle",
+            align: "right",
+            fontSize: 16,
+          },
+          leaves: {
+            label: {
+              position: "right",
+              verticalAlign: "middle",
+              align: "left",
+            },
+          },
+          emphasis: {
+            focus: "descendant",
+          },
+          expandAndCollapse: true,
+          animationDuration: 550,
+          animationDurationUpdate: 750,
+        },
+      ],
+    };
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
+
 <template>
   <VRow>
-    <VCol
-      cols="12"
-    >
+    <VCol cols="12">
       <v-table>
         <template v-if="!hide">
-          <!-- <Suspense> -->
-            <!-- <chart class="chart" :option="tempChart" :autoresize="true"></chart> -->
-            detailed chart
-          <!-- </Suspense> -->
+          <Suspense>
+            <Chart class="chart" :option="chartOptions" :autoresize="true"></Chart>
+          </Suspense>
         </template>
         <template v-else>
           <p>No activity to show</p>
@@ -47,12 +86,12 @@ type ChartOptions = ECBasicOption & { xAxis: any; yAxis: any };
       </v-table>
     </VCol>
   </VRow>
-  <p> 
+  <p>
     <hr>
   </p>
 </template>
 <style lang="scss" scoped>
 .chart {
-  height: 300px;
+  height: 600px;
 }
 </style>
